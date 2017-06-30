089e005d75b624bff8e9ff0ea31d0d582fadf00c
knou <kunodev@gmail.com>
2017-06-30 12:52:37 +0900

    [add] files

---
+++ QRGen/Example/QRGenExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QRGenExample : MonoBehaviour {
+
+	public RawImage qrImageView;
+
+	void Start () {
+		string inputText = "http://google.com";
+		Texture2D texture = new Texture2D (256, 256);
+		qrImageView.texture = texture;
+		QRGen.Instance.create (inputText,(Texture2D)qrImageView.texture);
+
+	}
+
+}
---
+++ QRGen/QRGen.cs
@@ -0,0 +1,35 @@
+﻿using ZXing;
+using ZXing.QrCode;
+using UnityEngine;
+
+public class QRGen : MonoBehaviour {
+
+	private static QRGen _instance;
+
+	public static QRGen Instance {
+		get { 
+			if (_instance == null) {
+				_instance = new QRGen ();
+			}
+			return _instance;
+		}
+	}
+
+	public void create(string inputString,Texture2D texture){
+		var qrCodeColors = Write (inputString, texture.width, texture.height);
+		texture.SetPixels32 (qrCodeColors);
+		texture.Apply ();
+	}
+
+	private Color32[] Write(string content,int width,int height){
+		var writer = new BarcodeWriter {
+			Format = BarcodeFormat.QR_CODE,
+			Options = new QrCodeEncodingOptions {
+				Height = height,
+				Width = width
+			}
+		};
+		return writer.Write (content);
+	}
+
+}
